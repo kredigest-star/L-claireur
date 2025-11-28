a18fa8a1ef34de2ac9de28364f1a4dd55eba5fc9
kredigest-star <kredigest@gmail.com>
2025-11-28 13:41:31 -0500

    Create config.js

---
+++ frontend/js/config.js
@@ -0,0 +1,15 @@
+const APP_CONFIG = {
+    name: "L'Éclaireur",
+    tagline: "La lumière sur l'actualité",
+    version: '1.0.0',
+    categories: [
+        { id: 'politique', name: 'Politique' },
+        { id: 'monde', name: 'Monde' },
+        { id: 'economie', name: 'Économie' },
+        { id: 'sciences', name: 'Sciences' },
+        { id: 'culture', name: 'Culture' },
+        { id: 'sports', name: 'Sports' }
+    ]
+};
+
+window.APP_CONFIG = APP_CONFIG;
